58841e3d862ef462c5873692b9813b0bfb450680
Dominic Frei <business.dominic.frei@gmail.com>
2020-09-06 23:38:25 +0100

    [1] Extract PlayerPrefs operations to a destinct helper class.

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections;
-using System.Globalization;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,12 +12,7 @@ public class GameState : MonoBehaviour
     private int _metal = 0;
     private int _crystal = 0;
     private int _deuterium = 0;
-
-    private readonly String _keyMetal = "key.metal";
-    private readonly String _keyCrystal = "key.crystal";
-    private readonly String _keyDeuterium = "key.deuterium";
-    private readonly String _keyLastUpdate = "key.lastUpdate";
-
+    
     void Start()
     {
         LoadResources();
@@ -35,8 +29,8 @@ public class GameState : MonoBehaviour
             _deuterium += UnityEngine.Random.Range(0, 5);
 
             UpdateInterface();
-            SaveResources();
-            SaveLastUpdateDate();
+            PlayerPrefsHelper.SaveResources(_metal, _crystal, _deuterium);
+            PlayerPrefsHelper.SaveLastUpdateDate();
 
             yield return new WaitForSeconds(1.0f);
         }
@@ -49,58 +43,31 @@ public class GameState : MonoBehaviour
         _textDeuterium.text = "Deuterium: " + _deuterium;
     }
 
-    private void SaveResources()
-    {
-        PlayerPrefs.SetInt(_keyMetal, _metal);
-        PlayerPrefs.SetInt(_keyCrystal, _crystal);
-        PlayerPrefs.SetInt(_keyDeuterium, _deuterium);
-    }
-
     private void LoadResources()
     {
-        _metal = PlayerPrefs.GetInt(_keyMetal);
-        _crystal = PlayerPrefs.GetInt(_keyCrystal);
-        _deuterium = PlayerPrefs.GetInt(_keyDeuterium);
-
+        (int metal, int crystal, int deuterium) = PlayerPrefsHelper.LoadResources();
         int secondsSinceLastUpdate = SecondsSinceLastUpdate();
 
-        _metal += secondsSinceLastUpdate;
-        _crystal += secondsSinceLastUpdate;
-        _deuterium += secondsSinceLastUpdate;
+        _metal = metal + secondsSinceLastUpdate;
+        _crystal = crystal + secondsSinceLastUpdate;
+        _deuterium = deuterium + secondsSinceLastUpdate;
 
         Debug.Log("Resources added: " + secondsSinceLastUpdate);
     }
 
-    private void SaveLastUpdateDate()
-    {
-        DateTime now = DateTime.Now;
-        DateTime nowUTC = now.ToUniversalTime();
-        String nowUTCString = nowUTC.ToString("O", CultureInfo.InvariantCulture);
-        PlayerPrefs.SetString(_keyLastUpdate, nowUTCString);
-    }
+    
 
     private int SecondsSinceLastUpdate()
     {
-        String lastUpdateUTCString = PlayerPrefs.GetString(_keyLastUpdate);
-
-        if ("" == lastUpdateUTCString)
+        DateTime? lastUpdateDate = PlayerPrefsHelper.LoadLastUpdateDate();
+        if (null == lastUpdateDate)
         {
-            Debug.LogWarning("Could not read lastUpdate. Probably never saved before.");
-            return 0;
-        }
-
-        TimeZoneInfo.ClearCachedData(); // just in case the time zone has changed
-        DateTime lastUpdateDate;
-        bool dateParsedSuccessfully = DateTime.TryParseExact(lastUpdateUTCString, "O", CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind, out lastUpdateDate);
-
-        if (!dateParsedSuccessfully)
-        {
-            Debug.LogError("Could not parse saved date.");
             return 0;
         }
 
         DateTime now = DateTime.Now.ToUniversalTime();
-        TimeSpan difference = now - lastUpdateDate;
+        DateTime unwrappedLastUpdateDate = lastUpdateDate ?? now;
+        TimeSpan difference = now - unwrappedLastUpdateDate;
         int differenceInSeconds = (int)difference.TotalSeconds;
         return differenceInSeconds;
     }
