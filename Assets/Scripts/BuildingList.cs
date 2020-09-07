35fff4e1f06c85ffd3e7a47985cccf1e8a997a0e
Dominic Frei <business.dominic.frei@gmail.com>
2020-09-07 01:10:11 +0100

    [4] Create a simple UI for some mines.

---
+++ Assets/Scripts/BuildingList.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingList : MonoBehaviour
+{
+    [SerializeField] private Text _textMetalMine = null;
+    [SerializeField] private Text _textCrystalMine = null;
+    [SerializeField] private Text _textDeuteriumMine = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _textMetalMine.text = "Metal Mine (Level 42)";
+        _textCrystalMine.text = "Crystal Mine (Level 42)";
+        _textDeuteriumMine.text = "Deuterium Mine (Level 42)";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/ResourceCounter.cs
+++ Assets/Scripts/ResourceCounter.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameState : MonoBehaviour
+public class ResourceCounter : MonoBehaviour
 {
     [SerializeField] private Text _textMetal = null;
     [SerializeField] private Text _textCrystal = null;
