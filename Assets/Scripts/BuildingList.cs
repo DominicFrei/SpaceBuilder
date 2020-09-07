7876224616b875b4ba8b4fb36feacccac2802510
Dominic Frei <business.dominic.frei@gmail.com>
2020-09-07 16:01:59 +0100

    [4] Update access modifier where possible.

--- Assets/Scripts/BuildingList.cs
+++ Assets/Scripts/BuildingList.cs
@@ -11,38 +11,50 @@ public class BuildingList : MonoBehaviour
     private int _crystalMineLevel = 1;
     private int _deuteriumMineLevel = 1;
 
-    void Start()
+    private void Start()
+    {
+        LoadBuildingData();
+        UpdateUI();
+    }
+
+    private void OnApplicationQuit()
+    {
+        PlayerPrefsHelper.SaveBuildingLevel(PlayerPrefsHelper.Building.metalMine, _metalMineLevel);
+        PlayerPrefsHelper.SaveBuildingLevel(PlayerPrefsHelper.Building.crystalMine, _crystalMineLevel);
+        PlayerPrefsHelper.SaveBuildingLevel(PlayerPrefsHelper.Building.deuteriumMine, _deuteriumMineLevel);
+    }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "OnClick() for button")]
+    private void MetalMineUpgradeClicked()
+    {
+        _metalMineLevel += 1;
+        UpdateUI();
+    }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "OnClick() for button")]
+    private void CrystalMineUpgradeClicked()
+    {
+        _crystalMineLevel += 1;
+        UpdateUI();
+    }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "OnClick() for button")]
+    private void DeuteriumMineUpgradeClicked()
+    {
+        _deuteriumMineLevel += 1;
+        UpdateUI();
+    }
+
+    private void LoadBuildingData()
     {
         _metalMineLevel = PlayerPrefsHelper.LoadBuildingLevel(PlayerPrefsHelper.Building.metalMine);
         _crystalMineLevel = PlayerPrefsHelper.LoadBuildingLevel(PlayerPrefsHelper.Building.crystalMine);
         _deuteriumMineLevel = PlayerPrefsHelper.LoadBuildingLevel(PlayerPrefsHelper.Building.deuteriumMine);
-        
+
         // Make sure the level is at least 1 (which could be 0 on the first load).
         _metalMineLevel = Mathf.Max(_metalMineLevel, 1);
         _crystalMineLevel = Mathf.Max(_crystalMineLevel, 1);
         _deuteriumMineLevel = Mathf.Max(_deuteriumMineLevel, 1);
-        UpdateUI();
-    }
-
-    public void MetalMineUpgradeClicked()
-    {
-        _metalMineLevel += 1;
-        PlayerPrefsHelper.SaveBuildingLevel(PlayerPrefsHelper.Building.metalMine, _metalMineLevel);
-        UpdateUI();
-    }
-
-    public void CrystalMineUpgradeClicked()
-    {
-        _crystalMineLevel += 1;
-        PlayerPrefsHelper.SaveBuildingLevel(PlayerPrefsHelper.Building.crystalMine, _crystalMineLevel);
-        UpdateUI();
-    }
-
-    public void DeuteriumMineUpgradeClicked()
-    {
-        _deuteriumMineLevel += 1;
-        PlayerPrefsHelper.SaveBuildingLevel(PlayerPrefsHelper.Building.deuteriumMine, _deuteriumMineLevel);
-        UpdateUI();
     }
 
     private void UpdateUI()
@@ -51,5 +63,4 @@ public class BuildingList : MonoBehaviour
         _textCrystalMine.text = "Crystal Mine (Level " + _crystalMineLevel + ")";
         _textDeuteriumMine.text = "Deuterium Mine (Level " + _deuteriumMineLevel + ")";
     }
-
 }
