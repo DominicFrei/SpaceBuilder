948c36d007a5b489cd2d93ea6e27f72a40d9d426
Dominic Frei <business.dominic.frei@gmail.com>
2020-09-08 10:20:07 +0100

    [5] Catch errors in database management.

--- Assets/Scripts/Database.cs
+++ Assets/Scripts/Database.cs
@@ -1,19 +1,30 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 public static class Database
 {
-    private static readonly string _savePath = Application.persistentDataPath + "resources.bin";
+    private static readonly string _savePath = Application.persistentDataPath + "/resources.bin";
 
     public static void SaveResources(int metal, int crystal, int deuterium)
     {
         BinaryFormatter binaryFormatter = new BinaryFormatter();
-
         FileStream fileStream = new FileStream(_savePath, FileMode.Create);
         ResourcesEntity resourcesEntity = new ResourcesEntity(metal, crystal, deuterium);
-        binaryFormatter.Serialize(fileStream, resourcesEntity);
-        fileStream.Close();
+        try
+        {
+            binaryFormatter.Serialize(fileStream, resourcesEntity);
+        }
+        catch (Exception exception)
+        {
+            Debug.LogError("Error during serialization: " + exception);
+        }
+        finally
+        {
+            fileStream.Close();
+        }        
+        Debug.Log("Data saved to " + _savePath);
     }
 
     public static (int metal, int crystal, int deuterium) LoadResources()
@@ -26,8 +37,21 @@ public static class Database
 
         BinaryFormatter binaryFormatter = new BinaryFormatter();
         FileStream fileStream = new FileStream(_savePath, FileMode.Open);
-        ResourcesEntity resourcesEntity = (ResourcesEntity)binaryFormatter.Deserialize(fileStream);
-        fileStream.Close();
+        ResourcesEntity resourcesEntity;
+        try
+        {
+            resourcesEntity = (ResourcesEntity)binaryFormatter.Deserialize(fileStream);
+        }
+        catch (Exception exception)
+        {
+            Debug.LogError("Error during deserialization: " + exception);
+            return (0, 0, 0);
+        }
+        finally
+        {
+            fileStream.Close();
+        }
+
         return (resourcesEntity.Metal, resourcesEntity.Crystal, resourcesEntity.Deuterium);
     }
 
