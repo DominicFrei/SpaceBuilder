0b69c6aaf03d95a90f322bb23e7df68e364a93e5
M.Kalavera <kalavera@gmail.com>
2020-09-12 20:54:22 +0200

    [8] MainMenu buttons with sprite swap

---
+++ Assets/Scripts/ButtonAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonAnimation : MonoBehaviour
+{
+    //variables
+    public Sprite btnUp;
+    public Sprite btnDown;
+    public Sprite btnHover;
+    public UnityEvent buttonClick;
+
+    void Awake()
+    {
+        if (buttonClick == null) { buttonClick = new UnityEvent(); }
+    }
+
+    void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().sprite = btnDown;
+    }
+    void OnMouseUp()
+    {
+        buttonClick.Invoke();
+        GetComponent<SpriteRenderer>().sprite = btnUp;
+    }
+}
