d62557faefda34cb6f9c6c5130427644765e177c
Dominic Frei <business.dominic.frei@gmail.com>
2020-09-12 23:25:00 +0100

    [7] Apply the MVC pattern to the resource counter.

---
+++ Assets/Scripts/ResourceCounter/ResourceCounterController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+public interface IResourceCounterController
+{
+    void Start();
+    void UpdateUI();
+    void OnApplicationQuit();
+}
+
+public class ResourceCounterController : IResourceCounterController
+{
+    private IResourceCounterView _resourcesCounterView = null;
+
+    private int _metal = 0;
+    private int _crystal = 0;
+    private int _deuterium = 0;
+
+    private int metalPerSecond = 20;
+    private int crystalPerSecond = 10;
+    private int deuteriumPerSecond = 5;
+
+    public ResourceCounterController(IResourceCounterView resourceCounterView)
+    {
+        _resourcesCounterView = resourceCounterView;
+    }
+
+    public void Start()
+    {
+        LoadResources();
+    }
+
+    public void UpdateUI()
+    {
+        _metal += metalPerSecond;
+        _crystal += crystalPerSecond;
+        _deuterium += deuteriumPerSecond;
+
+        if (null == _resourcesCounterView)
+        {
+            Logger.Error("_resourcesCounterView is null.");
+            return;
+        }
+        _resourcesCounterView.UpdateText(_metal, _crystal, _deuterium);
+    }
+
+    public void OnApplicationQuit()
+    {
+        Database.SaveResources(_metal, _crystal, _deuterium, DateTime.Now);
+    }
+
+    private void LoadResources()
+    {
+        (int metal, int crystal, int deuterium, DateTime lastUpdate) = Database.LoadResources();
+
+        int secondsSinceLastUpdate = DateHelper.DifferenceToNowInSeconds(lastUpdate);
+
+        int addedMetal = secondsSinceLastUpdate * metalPerSecond;
+        int addedCrystal = secondsSinceLastUpdate * crystalPerSecond;
+        int addedDeuterium = secondsSinceLastUpdate * deuteriumPerSecond;
+
+        _metal = metal + addedMetal;
+        _crystal = crystal + addedCrystal;
+        _deuterium = deuterium + addedDeuterium;
+
+        Logger.Info("Metal added: " + addedMetal);
+        Logger.Info("Crystal added: " + addedCrystal);
+        Logger.Info("Deuterium added: " + addedDeuterium);
+    }
+}
---
+++ Assets/Scripts/ResourceCounter/ResourceCounterView.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public interface IResourceCounterView
+{
+    void UpdateText(int metal, int crystal, int deuterium);
+}
+
+public class ResourceCounterView : MonoBehaviour, IResourceCounterView
+{
+    [SerializeField] private Text _textMetal = null;
+    [SerializeField] private Text _textCrystal = null;
+    [SerializeField] private Text _textDeuterium = null;
+
+    private IResourceCounterController _resourceCounterController = null;
+
+    private readonly WaitForSeconds _resourcesUpdateInterval = new WaitForSeconds(1.0f);
+
+    private void Start()
+    {
+        _resourceCounterController = new ResourceCounterController(this);
+        if (null == _resourceCounterController)
+        {
+            Logger.Error("_resourceCounterController is null.");
+            return;
+        }
+
+        _resourceCounterController.Start();
+        StartCoroutine(UpdateResourceCounter());
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (null == _resourceCounterController)
+        {
+            Logger.Error("_resourceCounterController is null.");
+            return;
+        }
+
+        _resourceCounterController.OnApplicationQuit();
+    }
+
+    private IEnumerator UpdateResourceCounter()
+    {
+        while (true)
+        {
+            if (null == _resourceCounterController)
+            {
+                Logger.Error("_resourceCounterController is null.");
+                yield break;
+            }
+
+            _resourceCounterController.UpdateUI();
+            yield return _resourcesUpdateInterval;
+        }
+    }
+
+    public void UpdateText(int metal, int crystal, int deuterium)
+    {
+        _textMetal.text = "Metal: " + metal;
+        _textCrystal.text = "Crystal: " + crystal;
+        _textDeuterium.text = "Deuterium: " + deuterium;
+    }    
+
+}
