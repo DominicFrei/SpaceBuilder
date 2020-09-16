7cb96649e1116f82149a4dbe3f87b66a29af60c2
M.Kalavera <kalavera@gmail.com>
2020-09-16 22:41:23 +0200

    [11] Options Menu with functional master volume slider

---
+++ Assets/Scripts/ShowValue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class ShowValue : MonoBehaviour
+{
+    TextMeshProUGUI percentageText;
+
+    void Start()
+    {
+        percentageText = GetComponent<TextMeshProUGUI>();
+    }
+    public void TextUpdate(float value)
+    {
+        percentageText.text = Mathf.RoundToInt(value * 100) + "%";
+    }
+}
