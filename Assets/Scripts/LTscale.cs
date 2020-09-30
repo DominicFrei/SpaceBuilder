cac3ff40f37dee724d61388db100378d1fd71297
M.Kalavera <kalavera@gmail.com>
2020-09-30 21:19:02 +0200

    [18] Animated logo with leantwean

--- Assets/Scripts/LTscale.cs
+++ Assets/Scripts/LTscale.cs
@@ -5,10 +5,23 @@ using UnityEngine.UIElements;
 
 public class LTscale: MonoBehaviour
 {
-    public float scaleSpeed = 10;
+    public float initSpeed = 0.01f;
+    public float scaleSpeed = 10f;
 
+    private Vector3 startSize = new Vector3(0, 0, 0);
+    private Vector3 normalSize = new Vector3(8, 8, 0);
+    private Vector3 scaleMax = new Vector3(10, 10, 4);
+    
     void Start()
     {
-    //    LeanTween.scale(gameObject.GetComponent<Transform>(Scale), new Vector3 (10f, 10f, 0f) , scaleSpeed);
+        ScaleZeroToSize();
     }
+
+    void ScaleZeroToSize()
+    {
+        LeanTween.scale(gameObject, startSize, 0.01f);
+        LeanTween.scale(gameObject, normalSize, 1f);
+        LeanTween.scale(gameObject, scaleMax, 1f);
+    }
+
 }
